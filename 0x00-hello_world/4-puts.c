6b14745988e7e92ea94eef1f4615d3b7c42cae54
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-16 13:26:44 -0700

    programming is like building m-lateral puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 
-/**
+/*
  *main -prints "program is like building a multilingualpuzzle  followed by a new line"
  *return 0.
- * * /
+ * /
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
