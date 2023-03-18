945231f924bcc2e1915ee4ad6972fa4ab9212663
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-18 14:23:58 -0700

    code to print positive,negative and zero when run

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* prints positive,zero or negative when run */
+/* prints positive,zero or negative when run 
+ * Return: Always(Success)
+ *
+ **/
 
 int main(void)
 {
@@ -9,16 +12,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if(n ==0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else 
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 
 	return(0);
 }
