0b3c72f78d6141943cbe73d52ef2c92e73a483ea
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-19 02:22:04 -0700

    print alphabets both capital and small letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/** main - prints alphabet in CAPITAL and small letters
+ * Return : Always(Success)
+ * 
+ */
+
+int main(void)
+{
+	char n;
+	for (n= 'a'; n <= 'z'; n++)
+	putchar(n);
+
+	for (n = 'A'; n <= 'Z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
