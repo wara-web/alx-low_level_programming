9f66edb2e6dd2907ca1a7ad6822c8a1110e6e27f
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-19 04:34:25 -0700

    code to print base 10 numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/* main - prints all digit numbers 0-10
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int h;
+
+	for (h = 0; h <= 9; h++)
+	printf("%d", h);
+	printf("\n");
+	return (0);
+}
+
