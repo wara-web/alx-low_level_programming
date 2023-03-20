a243653f01fedcd601c97d7ae56511f37ff90d1a
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-20 04:10:44 -0700

    a c program that prints  programming is like bulding a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,10 @@
  * followed by a new line"
  *Return:Always 0.
  */
-int main(void)
+
+int main(void
+
 {
-puts(" \"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
