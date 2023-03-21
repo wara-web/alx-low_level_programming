441053b13b6731f7b7ee8a2a755276c4977b0e33
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-21 06:51:06 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints_alphabet - make alphabets
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
