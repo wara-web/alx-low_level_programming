fe651d81f088df7f19d8cd65263709605771e021
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-28 04:22:12 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to a hint and updates
+ * the value points to 98
+ * @n: the integer input pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
