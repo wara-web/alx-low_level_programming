b67c7b22795b465af22d8971e68555965fbb5f23
Bill Clinton Okoth <okothensisb@gmail.com>
2023-03-30 06:23:25 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies at most an inputted number
+ * of bytes from string src into dest
+ * @dest: the buffer storing the string
+ * @src: the source string
+ * @n: the max number of bytes to copy from src
+ * Return: a apointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
