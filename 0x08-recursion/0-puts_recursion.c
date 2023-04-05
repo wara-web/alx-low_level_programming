7352fcc8acc9ff900bfa354e7fd33e5a459bcdd8
Bill Clinton Okoth <okothensisb@gmail.com>
2023-04-05 20:12:19 +0300

    1 print reverse recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -17,7 +17,7 @@ void _puts_recursion(char *s)
                 _puts_recursion(s + 1);                                                                                                 
         }                                                                                                                               
                                                                                                                                         
-        else                                                                                                                            
+        else                                                                                                                        
                 _putchar('\n');                                                                                                         
 }
 
